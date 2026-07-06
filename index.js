ac0fabef77ac51b62e95b0fd5980c299d2e7edaf
ganiyu <olagunjuayodeleganiyu@gmail.com>
2026-07-06 12:16:28 +0100

    Refactor start function to improve option list display and formatting

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 let title = document.getElementById("title");
 let options = document.getElementById("option-list");
 let startBtn = document.getElementById("start-btn");
+let optionList = document.querySelectorAll('#option-list li')
 
+
+
+
+let players = ["messi", "mbappe", "di maria", "kolo muani"]
 
 function start() {
 
     title.textContent = '1. Who scored the winning goal at world cup final 2022';
 
-    options.style.display = 'block'
+    options.style.display = 'block';
+
+    optionList.forEach((option, index) => {
+        option.textContent = players[index];
+    })
+
+
 }
 
 startBtn.addEventListener('click', start)

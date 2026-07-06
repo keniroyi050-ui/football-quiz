f277823617c0e06ec898e8ee8117136ee2fcc67e
Fareed <fareedademola2@gmail.com>
2026-07-06 12:40:54 +0100

    Refactor code style for consistency and readability in the start function

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 let title = document.getElementById("title");
 let options = document.getElementById("option-list");
 let startBtn = document.getElementById("start-btn");
-let optionList = document.querySelectorAll('#option-list li')
-let startScreen = document.querySelector('.start-screen')
+let optionList = document.querySelectorAll("#option-list li");
+let startScreen = document.querySelector(".start-screen");
 
-
-
-
-let players = ["messi", "mbappe", "di maria", "kolo muani"]
+let players = ["messi", "mbappe", "di maria", "kolo muani"];
 
 function start() {
+  title.textContent = "1. Who scored the winning goal at world cup final 2022";
 
-    title.textContent = '1. Who scored the winning goal at world cup final 2022';
+  options.style.display = "block";
 
-    options.style.display = 'block';
+  optionList.forEach((option, index) => {
+    option.textContent = players[index];
+  });
 
-    optionList.forEach((option, index) => {
-        option.textContent = players[index];
-    })
+  startBtn.style.display = "none";
 
-    startBtn.style.display = "none"
+  let myDiv = document.createElement("div");
 
-    let myDiv = document.createElement('div')
+  startScreen.appendChild(myDiv);
 
-    startScreen.appendChild(myDiv)
-
-    console.log(startScreen)
+  console.log(startScreen);
+  let button = document.createElement("button");
+  myDiv.appendChild(button);
+  console.log(button);
+  let button2 = document.createElement("button");
+  myDiv.appendChild(button2);
+  console.log(button2);
 }
 
-startBtn.addEventListener('click', start)
+startBtn.addEventListener("click", start);
